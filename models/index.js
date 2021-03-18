1fe9572108a90f42a9841683c1fbd8affefcca07
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-03-18 21:58:35 +0000

    Sequelize added to server/category routes added

--- models/index.js
+++ models/index.js
@@ -5,14 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.hasOne(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
-Category.belongsTo(Product, {
-  foreignKey: 'category_id',
-});
+// Category.belongsTo(Product, {
+//   foreignKey: 'category_id',
+// });
 
 // Categories have many Products
 Category.hasMany(Product, {
@@ -20,9 +20,9 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE',
 });
 
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-});
+// Product.belongsTo(Category, {
+//   foreignKey: 'category_id',
+// });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
