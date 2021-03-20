4f4c7c7ee9a8b428fbbb1aca95fdf3ddb17a5bab
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-03-20 14:58:07 +0000

    Showing categories with products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,8 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: { model: Tag, Category, through: ProductTag, as:'the_product_tag' }
+      // include: { model: Tag, Category, through: ProductTag, as:'the_product_tag' }
+      include: [{ model: Tag, through: ProductTag, as:'the_product_tag'}, { model: Category }]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -23,7 +24,9 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: { model: Tag, Category, through: ProductTag, as:'the_product_tag' }
+      // include: { model: Tag, Category, through: ProductTag, as:'the_product_tag' }
+      include: [{ model: Tag, through: ProductTag, as:'the_product_tag'}, { model: Category }]
+
     });
     if (!productData) {
       res.status(404).json({ message: 'No product found with this id!' });
